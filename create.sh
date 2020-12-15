7e40c216ee86d96a4630333986c8b91e17a9e4a3
Scarlettwolf-code <68124752+Scarlettwolf-code@users.noreply.github.com>
2020-12-14 22:31:48 -0800

    tyep
    fix types

--- create.sh
+++ create.sh
@@ -3,14 +3,16 @@
 curl -O https://v.firebog.net/hosts/csv.txt
 
 IFS=","
-while read -r type tick website name list
+while read -r typee tick website name list
 do
 	#create file name
 	filename="$(echo $name | tr -cd '[:alnum:]._-').dns"
+	website=$(echo $website | tr -d '"')
+	typee=$(echo $typee | tr -d '"')
 	#crate header
 	echo -e "#\n# $name\n#\n#" > "$filename"
 	echo "# Maintainer	: $website" >> "$filename"
-	echo "# Category        : $type" >> "$filename"
+	echo "# Category        : $typee" >> "$filename"
 	echo "# Confidence      : 4 " >> "$filename"
 	list=$(echo $list | tr -d '"')
 	curl $list >> "$filename"
