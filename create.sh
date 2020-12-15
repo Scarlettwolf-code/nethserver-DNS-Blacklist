b561d7a80d8d2ee875fa39155927569e4e09ea50
Scarlettwolf-code <68124752+Scarlettwolf-code@users.noreply.github.com>
2020-12-14 22:16:32 -0800

    all firebog list
    Puted the csv use it to downlaod all lists in it

---
+++ create.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#get list of lists
+curl -O https://v.firebog.net/hosts/csv.txt
+
+IFS=","
+while read -r type tick website name list
+do
+	#create file name
+	filename="$(echo $name | tr -cd '[:alnum:]._-').dns"
+	#crate header
+	echo -e "#\n# $name\n#\n#" > "$filename"
+	echo "# Maintainer	: $website" >> "$filename"
+	echo "# Category        : $type" >> "$filename"
+	echo "# Confidence      : 4 " >> "$filename"
+	list=$(echo $list | tr -d '"')
+	curl $list >> "$filename"
+	sed -i '/#/d' "$filename"
+        sed -i '/localhost/d' "$filename" 
+	sed -i '/^$/d' "$filename" 
+	sed -i 's/127.0.0.1//g' "$filename"
+       	sed -i 's/0.0.0.0//g' "$filename"
+	sed -i '/<*>/d' "$filename"
+
+done < csv.txt
