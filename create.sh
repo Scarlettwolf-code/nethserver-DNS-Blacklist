89518168d9160b39219d414fe067519267d128e3
Scarlettwolf-code <68124752+Scarlettwolf-code@users.noreply.github.com>
2020-12-17 10:21:00 -0800

    add function
    function and forks

--- create.sh
+++ create.sh
@@ -2,19 +2,19 @@
 #get list of lists
 curl -O https://v.firebog.net/hosts/csv.txt
 
-IFS=","
-while read -r typee tick website name list
-do
+create_file () 
+{
+
 	#create file name
-	filename="$(echo $name | tr -cd '[:alnum:]._-').dns"
-	website=$(echo $website | tr -d '"')
-	typee=$(echo $typee | tr -d '"')
+	filename="$(echo $1 | tr -cd '[:alnum:]._-').dns"
+	website=$(echo $2 | tr -d '"')
+	typee=$(echo $3 | tr -d '"')
+	list=$(echo $4 | tr -d '"')
 	#crate header
 	echo -e "#\n# $name\n#\n#" > "$filename"
 	echo "# Maintainer	: $website" >> "$filename"
 	echo "# Category        : $typee" >> "$filename"
 	echo "# Confidence      : 4 " >> "$filename"
-	list=$(echo $list | tr -d '"')
 	curl $list >> "$filename"
 	#sed -i '/#/d' "$filename"
         sed -i '/localhost/d' "$filename" 
@@ -22,5 +22,14 @@ do
 	sed -i 's/127.0.0.1//g' "$filename"
        	sed -i 's/0.0.0.0//g' "$filename"
 	sed -i '/<*>/d' "$filename"
+}
+IFS=","
+while read -r typee tick website name list
+do
+
+create_file $name $website $typee $list &
 
 done < csv.txt
+
+wait
+exit
