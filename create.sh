e03214f3ddc2b540b80fc52f66758a987e54189a
root <root@adept.scarlettwolf.website>
2021-02-02 19:59:59 -0800

    update for Tue Feb  2 19:59:38 PST 2021

--- create.sh
+++ create.sh
@@ -1,35 +1,3 @@
-#!/bin/bash
-#get list of lists
-#curl -O https://v.firebog.net/hosts/csv.txt
-
-create_file () 
-{
-
-	#create file name
-	filename="$(echo $1 | tr -cd '[:alnum:]._-').dns"
-	website=$(echo $2 | tr -d '"')
-	typee=$(echo $3 | tr -d '"')
-	list=$(echo $4 | tr -d '"')
-	#create header
-	echo -e "#\n# $name\n#\n#" > "$filename"
-	echo "# Maintainer	: $website" >> "$filename"
-	echo "# Category        : $typee" >> "$filename"
-	echo "# Confidence      : 4 " >> "$filename"
-	curl -# --connect-timeout 30 --max-time 90 $list >> "$filename"
-	#sed -i '/#/d' "$filename"
-        sed -i '/localhost/d' "$filename" 
-	sed -i '/^$/d' "$filename" 
-	sed -i 's/127.0.0.1//g' "$filename"
-       	sed -i 's/0.0.0.0//g' "$filename"
-	sed -i '/<*>/d' "$filename"
-}
-
-IFS=","
-while read -r typee tick website name list
-do
-
-create_file $name $website $typee $list
-
-done < /git/nethserver-DNS-Blacklist/csv.txt
-
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:c7db4b7b58a69011b8f7ef8a20b7f8d720de9d4c25934d2dd219fe77344852cb
+size 838
